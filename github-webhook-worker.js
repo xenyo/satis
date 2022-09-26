d0920e7678f83b168161b1437060aa4df328f7eb
Parkle Lee <parkle.lee@xenyo.com>
2022-09-26 17:51:08 +0800

    Use xenyo/github-webhook-worker

---
+++ github-webhook-worker.js
@@ -0,0 +1,22 @@
+const { workerData: event, parentPort } = require("worker_threads");
+const { exec } = require("child_process");
+
+const { name, url } = event.data.payload.repository;
+
+let command = "";
+
+if (name === "satis") {
+  command = "git diff HEAD --exit-code && bin/satis build";
+} else if (event.data.payload.deleted) {
+  command = "bin/satis build";
+} else {
+  command = `bin/satis build --repository-url ${new URL(url).toString()}`;
+}
+
+parentPort.postMessage(command);
+
+exec(command, { cwd: __dirname }, (error, stdout, stderr) => {
+  if (error) throw error;
+  if (stderr) parentPort.postMessage(stderr);
+  if (stdout) parentPort.postMessage(stdout);
+});
