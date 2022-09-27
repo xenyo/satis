4853a20b9e84be418fcdd375064f0db07b06a0d7
Parkle Lee <parkle.lee@xenyo.com>
2022-09-27 11:40:43 +0800

    Fix formatting

--- webhooks.js
+++ webhooks.js
@@ -13,45 +13,43 @@ dotenv.config();
 
 // Set up loglevel
 if (process.env.GITHUB_WEBHOOKS_LOG_LEVEL)
-    log.setLevel(process.env.GITHUB_WEBHOOKS_LOG_LEVEL);
+log.setLevel(process.env.GITHUB_WEBHOOKS_LOG_LEVEL);
 
 // Set up queue
 const queue = fastq((command, cb) => {
-    log.info(command);
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            log.error(error);
-            return cb(error);
-        }
-        if (stderr) {
-            log.error(stderr);
-            return cb(error);
-        }
-        log.debug(stdout);
-        cb(null, stdout);
-    });
+  log.info(command);
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      log.error(error);
+      return cb(error);
+    }
+    if (stderr) {
+      log.error(stderr);
+      return cb(error);
+    }
+    log.debug(stdout);
+    cb(null, stdout);
+  });
 });
 
 // Set up webhook handler
 const webhooks = new Webhooks({ secret: process.env.GITHUB_WEBHOOKS_SECRET });
 webhooks.on("push", (event) => {
-    // Pull and trigger full build if Satis is updated
-    if (event.payload.repository.name === "satis") {
-        queue.kill();
-        return queue.push(
-            "git diff HEAD --exit-code && git pull && bin/satis build"
-        );
-    }
-
-    // Trigger full build if a branch or tag is deleted
-    if (event.payload.deleted) {
-        queue.kill();
-        return queue.push("bin/satis build");
-    }
-
-    // Trigger partial build
-    const url = new URL(event.payload.repository.url).toString();
-    queue.push(`bin/satis build --repository-url ${url}`);
+  // Pull and trigger full build if Satis is updated
+  if (event.payload.repository.name === "satis") {
+    queue.kill();
+    return queue.push("git diff HEAD --exit-code && git pull && bin/satis build");
+  }
+    
+  // Trigger full build if a branch or tag is deleted
+  if (event.payload.deleted) {
+    queue.kill();
+    return queue.push("bin/satis build");
+  }
+    
+  // Trigger partial build
+  const url = new URL(event.payload.repository.url).toString();
+  queue.push(`bin/satis build --repository-url ${url}`);
 });
 
 // Start http server
