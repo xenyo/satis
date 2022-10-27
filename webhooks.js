e4ae8c757c55306e70c748faa2327b5ee3f4010e
Parkle Lee <parkle.lee@xenyo.com>
2022-10-27 13:14:59 +0800

    Switch to github-webhooks-exec

---
+++ webhooks.js
@@ -0,0 +1,28 @@
+const { queue, server, webhooks } = require('github-webhooks-exec');
+
+const pino = require('pino');
+
+const logger = pino();
+
+webhooks.on('push', event => {
+  logger.info('Received push event');
+
+  // Pull and trigger full build if Satis is updated
+  if (event.payload.repository.name === "satis") {
+    queue.kill();
+    return queue.push("git diff HEAD --exit-code && git pull && bin/satis build");
+  }
+
+  // Trigger full build if a branch or tag is deleted
+  if (event.payload.deleted) {
+    queue.kill();
+    return queue.push("bin/satis build");
+  }
+
+  // Trigger partial build
+  const url = new URL(event.payload.repository.url).toString();
+  queue.push(`bin/satis build --repository-url ${url}`);
+});
+
+server.listen(process.env.GITHUB_WEBHOOKS_PORT);
+logger.info('Ready');
