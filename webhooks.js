7bac365ae21946b580ae266cf5b423f97a03543a
Parkle Lee <parkle.lee@xenyo.com>
2022-11-01 18:05:59 +0800

    Update webhooks script

--- webhooks.js
+++ webhooks.js
@@ -1,28 +1,26 @@
-const { queue, server, webhooks } = require('github-webhooks-exec');
+const { queue, server, webhooks, logger } = require("github-webhooks-exec");
 
-const pino = require('pino');
+webhooks.on("push", (event) => {
+    logger.info("Received push event");
 
-const logger = pino();
+    // Pull and trigger full build if Satis is updated
+    if (event.payload.repository.name === "satis") {
+        queue.kill();
+        return queue.push(
+            "git diff HEAD --exit-code && git pull && bin/satis build"
+        );
+    }
 
-webhooks.on('push', event => {
-  logger.info('Received push event');
+    // Trigger full build if a branch or tag is deleted
+    if (event.payload.deleted) {
+        queue.kill();
+        return queue.push("bin/satis build");
+    }
 
-  // Pull and trigger full build if Satis is updated
-  if (event.payload.repository.name === "satis") {
-    queue.kill();
-    return queue.push("git diff HEAD --exit-code && git pull && bin/satis build");
-  }
-
-  // Trigger full build if a branch or tag is deleted
-  if (event.payload.deleted) {
-    queue.kill();
-    return queue.push("bin/satis build");
-  }
-
-  // Trigger partial build
-  const url = new URL(event.payload.repository.url).toString();
-  queue.push(`bin/satis build --repository-url ${url}`);
+    // Trigger partial build
+    const url = new URL(event.payload.repository.url).toString();
+    queue.push(`bin/satis build --repository-url ${url}`);
 });
 
 server.listen(process.env.GITHUB_WEBHOOKS_PORT);
-logger.info('Ready');
+logger.info("Ready");
