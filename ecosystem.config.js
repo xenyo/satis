57b476b7713c0ab43938a520f0faf012bf39af33
Parkle Lee <parkle.lee@xenyo.com>
2022-11-02 09:28:35 +0800

    Disable husky and reformat

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    apps: [
-        {
-            name: "satis-webhooks",
-            script: "./webhooks.js",
-        },
-    ],
+  apps: [
+    {
+      name: 'satis-webhooks',
+      script: './webhooks.js',
+    },
+  ],
 };
--- webhooks.js
+++ webhooks.js
@@ -1,41 +1,35 @@
-const { queue, server, webhooks, logger } = require("github-webhooks-exec");
+const { queue, server, webhooks, logger } = require('github-webhooks-exec');
 
-webhooks.on("push", (event) => {
-    // Pull and trigger full build if Satis is updated
-    if (event.payload.repository.name === "satis") {
-        logger.info(
-            `${event.payload.ref} was updated in ${event.payload.repository.full_name}.`
-        );
-        queue.kill();
-        const commands = [
-            "git diff HEAD --exit-code",
-            "git pull",
-            "composer install",
-            "bin/satis build",
-            "pnpm install",
-            "pm2 reload ecosystem.config.js",
-        ];
-        queue.push(commands.join(" && "));
-        return;
-    }
+webhooks.on('push', event => {
+  // Pull and trigger full build if Satis is updated
+  if (event.payload.repository.name === 'satis') {
+    logger.info(`${event.payload.ref} was updated in ${event.payload.repository.full_name}.`);
+    queue.kill();
+    const commands = [
+      'git diff HEAD --exit-code',
+      'git pull',
+      'composer install',
+      'bin/satis build',
+      'pnpm install',
+      'pm2 reload ecosystem.config.js',
+    ];
+    queue.push(commands.join(' && '));
+    return;
+  }
 
-    // Trigger full build if a branch or tag is deleted
-    if (event.payload.deleted) {
-        logger.info(
-            `${event.payload.ref} was deleted from ${event.payload.repository.full_name}.`
-        );
-        logger.info("A branch or tag was deleted");
-        queue.push("bin/satis build");
-        return;
-    }
+  // Trigger full build if a branch or tag is deleted
+  if (event.payload.deleted) {
+    logger.info(`${event.payload.ref} was deleted from ${event.payload.repository.full_name}.`);
+    logger.info('A branch or tag was deleted');
+    queue.push('bin/satis build');
+    return;
+  }
 
-    // Trigger partial build
-    logger.info(
-        `${event.payload.ref} was updated in ${event.payload.repository.full_name}.`
-    );
-    const url = new URL(event.payload.repository.url).toString();
-    queue.push(`bin/satis build --repository-url ${url}`);
+  // Trigger partial build
+  logger.info(`${event.payload.ref} was updated in ${event.payload.repository.full_name}.`);
+  const url = new URL(event.payload.repository.url).toString();
+  queue.push(`bin/satis build --repository-url ${url}`);
 });
 
 server.listen(process.env.GITHUB_WEBHOOKS_PORT);
-logger.info("Ready");
+logger.info('Ready');
